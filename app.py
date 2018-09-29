4e6f64e9f4b0563183bed9b94b8e76c3389d367a
Zsofia Limbek <zsofia.limbek@gmail.com>
2018-09-29 20:20:05 +0200

    added game starter & game grid

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/", methods=["POST"])
+def game():
+    number_of_cards = int(request.form["number_of_cards"])
+    return render_template("game.html", number_of_cards=number_of_cards)
 
 
 if __name__ == "__main__":
